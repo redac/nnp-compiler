46cc160e2d8e2e7f24e20f5ed6ff3e93458e23f4
LeLoukas <lduque@enssat.fr>
2022-04-29 16:01:22 +0200

    codeGenerator

---
+++ src/codeGenerator.py
@@ -0,0 +1,16 @@
+# Code relatif à la génération de code
+from numpy import void
+
+
+class codeGenerator:
+	
+	# Constructeur
+	def __init__(self):
+		self.generatedCode = []
+		instrIndex = 0
+	
+
+	########### Méthodes ###########
+
+	def genererCode(self, chaineCodee):
+		return void
