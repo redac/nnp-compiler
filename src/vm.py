396ff7f0a60fda92b6b5695a8f322ba7cd048d8a
pbouttes <pbouttes@gmail.com>
2022-05-09 12:11:58 +0200

    added affectation

--- src/vm.py
+++ src/vm.py
@@ -2,7 +2,9 @@
 import argparse
 import re
 
+
 reg_reserver = re.compile("reserver\(([0-9]+)\)")
+reg_empiler = re.compile("empiler\(([0-9]+)\)")
 
 
 class Pile:
@@ -26,7 +28,12 @@ class Pile:
             ch = "|\t" + str(x) + "\t|" + "\n" + ch
         ch = "\nEtat de la pile:\n" + ch
         return ch
+class VMException(Exception):
+    def __init__(self, value):
+        self.value = value
 
+    def __str__(self):
+        return repr(self.value)
 
 class virtual_machine:
 
@@ -40,11 +47,38 @@ class virtual_machine:
         # Partie programme
         self.co = 0  # compteur ordinal
         print("VM initialised")
-        
 
-    def reserver(self,n):
-        self.pointeur += n
-        print(str(n)+" emplacement(s) réservés")
+    def reserver(self, n):
+        if n >= 0:
+            self.pointeur += n
+            print(str(n)+" emplacement(s) réservés")
+        else:
+            raise VMException(
+                "Reserver: le nombre d'emplacements à réserver doit être positif")
+
+    def empiler(self, val):
+        self.pile.empiler(val)
+        self.pointeur += 1
+        print(str(val)+" empilé")
+
+    def affectation(self):
+        val = self.pile.depiler()  #on récupère la valeur à affecter
+        ad = self.pile.depiler()   #on récupère l'adresse à affecter
+        #préconditions
+        if self.pointeur<ad+2:
+          raise VMException("Affectation: adresse incorrecte")
+        
+        temp = Pile()
+        print("adresse:" + str(ad)+ "   valeur: "+str(val))
+        print(str(self.pile))
+        while len(self.pile.valeurs) != ad:
+            temp.empiler(self.pile.depiler)   #on empile toute la pile dans une pile temporaire
+        self.pile.empiler(val)      #on met la valeur à l'adresse indiquée
+        temp.depiler()    #on enlève l'ancienne valeur de la variable
+        while not temp.estVide(): 
+          self.pile.empiler(temp.depiler)  #on rempile la pile
+        self.pointeur-=2
+        print ("Valeur :"+str(val)+" affectée à l'adresse: "+str(ad))
 
     def analyse(self, l):
 
@@ -52,9 +86,11 @@ class virtual_machine:
             self.init_analyser()
         elif reg_reserver.match(l):
             self.reserver(int(reg_reserver.match(l).group(1)))
-
-
-            
+        elif reg_empiler.match(l):
+            self.empiler(int(reg_empiler.match(l).group(1)))
+        elif l == "affectation()":
+            self.affectation()
+        # print(str(self.pile))
 
 
 def main():
@@ -75,12 +111,12 @@ def main():
     vm = virtual_machine()
 
     for line in f:
-            line = line.rstrip('\r\n')
-            # print(str(line))
-            if line != "finProg();":
-                vm.analyse(line)
-            else:
-              return
+        line = line.rstrip('\r\n')
+        print(str(line))
+        if line != "finProg();":
+            vm.analyse(line)
+        else:
+            return
 
     f.close()
 
